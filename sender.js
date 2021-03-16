822bf9f8206f70777d0e1c08698caa31593b6bde
Nat Weerawan <nat.wrw@gmail.com>
2021-03-16 19:01:24 +0700

    Initial commit

---
+++ sender.js
@@ -0,0 +1,55 @@
+const mqtt = require("mqtt");
+
+let xq = [
+  'MQTT_HOST',
+  'MQTT_PORT',
+  'MQTT_USERNAME',
+  'MQTT_PASSWORD',
+  'MQTT_PREFIX',
+]
+
+let xx = xq.map((k) => Object.keys(process.env).includes(k))
+
+for (const item of xq) {
+  console.log('>', process.env[item] ? '[found]' : '', item, ':', process.env[item])
+}
+
+xx = xx.filter((x) => x == true)
+
+if (xx.length !== xq.length) {
+  console.error('Invalid MQTT arguments')
+  process.exit(-1)
+}
+
+const client = mqtt.connect({
+  host: process.env.MQTT_HOST,
+  port: process.env.MQTT_PORT,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  // will: {
+  //   topic: `lwt/processing.js/${process.env.PROCESSOR_ID}`,
+  //   payload: 'DEAD',
+  //   qos: 1,
+  //   retain: true,
+  // },
+})
+
+client.on("connect", function () {
+  console.log(`mqtt connected.`);
+  //   client.subscribe("presence", function (err) {
+  //     if (!err) {
+  //         console.lo
+  //     }
+  //   });
+});
+
+client.on("message", function (topic, message) {
+  console.log(message.toString());
+});
+
+module.exports = {
+  client,
+  publish: (topic, message, opts) => {
+    client.publish(topic, message, opts);
+  },
+};
